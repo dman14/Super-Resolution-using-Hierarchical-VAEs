499b575003e2557ff36b9290fb04e19575ce60b2
dman14 <darius.chira@yahoo.com>
2020-11-19 15:04:38 +0000

    "rescale function"

--- helper.py
+++ helper.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from torch import nn, optim
 from torch.autograd import Variable
-
+from torchvision import datasets, transforms
+import PIL.Image as pil_image
 
 def test_network(net, trainloader):
 
@@ -53,45 +54,21 @@ def imshow(image, ax=None, title=None, normalize=True):
 
     return ax
 
+def rescale(image, scale=4):
+  to_pil_image = transforms.ToPILImage()
+  
+  hr = to_pil_image(image)
+  hr_width = (hr.width // scale) * scale
+  hr_height = (hr.height // scale) * scale
+  hr = hr.resize((hr_width, hr_height), resample=pil_image.BICUBIC)
+  lr = hr.resize((hr_width // scale, hr_height // scale), resample=pil_image.BICUBIC)
+  lr = lr.resize((lr.width * scale, lr.height * scale), resample=pil_image.BICUBIC)
 
-def view_recon(img, recon):
-    ''' Function for displaying an image (as a PyTorch Tensor) and its
-        reconstruction also a PyTorch Tensor
-    '''
+  pil_to_tensor = transforms.ToTensor()(hr).unsqueeze_(0)
+  tensor_to_pil = transforms.ToPILImage()(pil_to_tensor.squeeze_(0))
+  hr = pil_to_tensor
 
-    fig, axes = plt.subplots(ncols=2, sharex=True, sharey=True)
-    axes[0].imshow(img.numpy().squeeze())
-    axes[1].imshow(recon.data.numpy().squeeze())
-    for ax in axes:
-        ax.axis('off')
-        ax.set_adjustable('box-forced')
-
-def view_classify(img, ps, version="MNIST"):
-    ''' Function for viewing an image and it's predicted classes.
-    '''
-    ps = ps.data.numpy().squeeze()
-
-    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
-    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
-    ax1.axis('off')
-    ax2.barh(np.arange(10), ps)
-    ax2.set_aspect(0.1)
-    ax2.set_yticks(np.arange(10))
-    if version == "MNIST":
-        ax2.set_yticklabels(np.arange(10))
-    elif version == "Fashion":
-        ax2.set_yticklabels(['T-shirt/top',
-                            'Trouser',
-                            'Pullover',
-                            'Dress',
-                            'Coat',
-                            'Sandal',
-                            'Shirt',
-                            'Sneaker',
-                            'Bag',
-                            'Ankle Boot'], size='small');
-    ax2.set_title('Class Probability')
-    ax2.set_xlim(0, 1.1)
-
-    plt.tight_layout()
-
+  pil_to_tensor2 = transforms.ToTensor()(lr).unsqueeze_(0)
+  tensor_to_pil2 = transforms.ToPILImage()(pil_to_tensor2.squeeze_(0))
+  lr = pil_to_tensor2
+  return(hr,lr)

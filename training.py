13420387a126b8bdd1a966976070b5b1751f7124
Darius <dman140@gmail.com>
2020-11-26 16:39:42 +0000

    added vae training files

--- training.py
+++ training.py
@@ -11,6 +11,10 @@ import torch.optim as optim
 import torch.nn.init as init
 from torch.nn import Linear, Conv2d, BatchNorm2d, MaxPool2d, Dropout2d
 from torch.nn.functional import relu, elu, relu6, sigmoid, tanh, softmax
+from collections import defaultdict
+
+from git.vae import *
+from git.plotting import *
 
 def training_init(net):
 
@@ -122,9 +126,87 @@ def training_cnn(net, train_loader, test_loader, num_epochs = 100 ):
         #                    epoch=epoch,
         #                    classes=classes,
         #                    dimensionality_reduction_op = None) #lambda z: TSNE(n_components=2).fit_transform(z))
-        print("on epoch:",epoch,"training loss:"train_loss[-1],"and validation loss:",valid_loss[-1])
+        print("on epoch:",epoch)#,"training loss:"train_loss[-1],"and validation loss:",valid_loss[-1])
 
     print(train_loss)
     print(valid_loss)
-    return net
+
+def vae_init(dataloader):
+
+    # define the models, evaluator and optimizer
+
+    images, low_res = next(iter(dataloader))
+
+    # VAE
+    latent_features = 2
+    vae = VariationalAutoencoder(images[0].shape, latent_features)
+
+    # Evaluator: Variational Inference
+    beta = 1
+    vi = VariationalInference(beta=beta)
+
+    # The Adam optimizer works really well with VAEs.
+    optimizer = torch.optim.Adam(vae.parameters(), lr=1e-3)
+
+    # define dictionary to store the training curves
+    training_data = defaultdict(list)
+    validation_data = defaultdict(list)
+    return vae, training_data, validation_data, vi, optimizer
+
         
+def training_vae(train_loader, test_loader, num_epochs = 100 ):
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f">> Using device: {device}")
+
+    vae, training_data, validation_data, vi, optimizer=vae_init(dataloader = train_loader)
+
+    # move the model to the device
+    vae = vae.to(device)
+
+    # training..
+    epoch =0
+    while epoch < num_epochs:
+        epoch+= 1
+        training_epoch_data = defaultdict(list)
+        vae.train()
+        
+        # Go through each batch in the training dataset using the loader
+        # Note that y is not necessarily known as it is here
+        for x, y in train_loader:
+            x = x.to(device)
+            y = y.to(device)
+            
+            # perform a forward pass through the model and compute the ELBO
+            loss, diagnostics, outputs = vi(vae, x)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            # gather data for the current bach
+            for k, v in diagnostics.items():
+                training_epoch_data[k] += [v.mean().item()]
+                
+
+        # gather data for the full epoch
+        for k, v in training_epoch_data.items():
+            training_data[k] += [np.mean(training_epoch_data[k])]
+
+        # Evaluate on a single batch, do not propagate gradients
+        with torch.no_grad():
+            vae.eval()
+            
+            # Just load a single batch from the test loader
+            x, y = next(iter(test_loader))
+            x = x.to(device)
+            
+            # perform a forward pass through the model and compute the ELBO
+            loss, diagnostics, outputs = vi(vae, x)
+            
+            # gather data for the validation step
+            for k, v in diagnostics.items():
+                validation_data[k] += [v.mean().item()]
+        
+        # Reproduce the figure from the begining of the notebook, plot the training curves and show latent samples
+        make_vae_plots(vae, x, y, outputs, training_data, validation_data)
